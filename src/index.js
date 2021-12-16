c9a2d5b234a21506c84c57992909c87d394df669
jilei-hao <jilei.cs@gmail.com>
2021-12-16 12:11:02 -0500

    hubmap-1: color rendering work

--- src/index.js
+++ src/index.js
@@ -1,13 +1,87 @@
 import '@kitware/vtk.js/Rendering/Profiles/Geometry';
 
+import vtkPolyDataReader from '@kitware/vtk.js/IO/Legacy/PolyDataReader';
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
 
-import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkCalculator      from '@kitware/vtk.js/Filters/General/Calculator';
-import vtkConeSource      from '@kitware/vtk.js/Filters/Sources/ConeSource';
-import { AttributeTypes } from '@kitware/vtk.js/Common/DataModel/DataSetAttributes/Constants';
-import { FieldDataTypes } from '@kitware/vtk.js/Common/DataModel/DataSet/Constants';
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
+import vtkPolyData from '@kitware/vtk.js/Common/DataModel/PolyData';
+import vtkDataArray from '@kitware/vtk.js/Common/Core/DataArray';
+import vtkLookupTable from '@kitware/vtk.js/Common/Core/LookupTable';
+
+import {
+  ColorMode,
+  ScalarMode,
+} from '@kitware/vtk.js/Rendering/Core/Mapper/Constants'
+
+import { AttributeTypes } from '@kitware/vtk.js/Common/DataModel/DataSetAttributes/Constants'
+
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
+
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
+const polyMapper = vtkMapper.newInstance();
+const polyActor = vtkActor.newInstance();
+
+// ----------------------------------------------------------------------------
+// Get input from the server
+// ----------------------------------------------------------------------------
+const reader = vtkPolyDataReader.newInstance();
+
+// url of the file server, should be in a configuration file
+const server_url = 'http://192.168.4.23:3000';
+const file_name = 'Ovary.vtk';
+
+// connect the rendering pipeline
+polyMapper.setInputConnection(reader.getOutputPort());
+polyActor.setMapper(polyMapper);
+renderer.addActor(polyActor);
+
+reader.setUrl(`${server_url}/${file_name}`).then(() => {
+  
+  reader.loadData().then(() => {
+    let polyData = reader.getOutputData();
+    let pointData = polyData.getPointData();
+
+    // Copy scalar data into color array
+    // - vtk.js color rendering by scalar only works for Int32Array
+    let pts = polyData.getPoints();
+    const clr = new Int32Array(pts.getNumberOfPoints());
+    const orig = pointData.getArrayByName('Label').getData();
+    for (let i = 0; i < clr.length; ++i) {
+      clr[i] = orig[i];
+    }
+
+    polyMapper.setScalarRange(0, 16);
+    
+    pointData.setScalars(
+      vtkDataArray.newInstance({
+        name: 'color',
+        numberOfComponents: 1,
+        values: clr,
+      })
+    )
+
+    console.log('color scalars created');
+
+    // --Debug: export local variable to global scope
+    global.polyData = polyData;
+    global.pointData = pointData;
+
+    renderer.resetCamera();
+    renderWindow.render();
+  });
+});
+
+renderer.resetCamera();
+renderWindow.render();
+
+// -----------------------------------------------------------
+// UI control handling
+// -----------------------------------------------------------
 
 const controlPanel = `
 <table>
@@ -20,71 +94,20 @@ const controlPanel = `
       </select>
     </td>
   </tr>
-  <tr>
-    <td>
-      <input class="resolution" type="range" min="4" max="80" value="6" />
-    </td>
-  </tr>
 </table>
 `;
 
-// ----------------------------------------------------------------------------
-// Standard rendering code setup
-// ----------------------------------------------------------------------------
-
-const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
-const renderer = fullScreenRenderer.getRenderer();
-const renderWindow = fullScreenRenderer.getRenderWindow();
-
-// ----------------------------------------------------------------------------
-// Example code
-// ----------------------------------------------------------------------------
-
-const coneSource = vtkConeSource.newInstance({ height: 1.0 });
-const filter = vtkCalculator.newInstance();
-
-filter.setInputConnection(coneSource.getOutputPort());
-filter.setFormula({
-  getArrays: inputDataSets => ({
-    input: [],
-    output: [
-      { location: FieldDataTypes.CELL, name: 'Random', dataType: 'Float32Array', attribute: AttributeTypes.SCALARS },
-    ],
-  }),
-  evaluate: (arraysIn, arraysOut) => {
-    const [scalars] = arraysOut.map(d => d.getData());
-    for (let i = 0; i < scalars.length; i++) {
-      scalars[i] = Math.random();
-    }
-  },
-});
-
-const mapper = vtkMapper.newInstance();
-mapper.setInputConnection(filter.getOutputPort());
-
-const actor = vtkActor.newInstance();
-actor.setMapper(mapper);
-
-renderer.addActor(actor);
-renderer.resetCamera();
-renderWindow.render();
-
-// -----------------------------------------------------------
-// UI control handling
-// -----------------------------------------------------------
-
 fullScreenRenderer.addController(controlPanel);
 const representationSelector = document.querySelector('.representations');
-const resolutionChange = document.querySelector('.resolution');
 
 representationSelector.addEventListener('change', (e) => {
   const newRepValue = Number(e.target.value);
-  actor.getProperty().setRepresentation(newRepValue);
+  polyActor.getProperty().setRepresentation(newRepValue);
   renderWindow.render();
 });
 
-resolutionChange.addEventListener('input', (e) => {
-  const resolution = Number(e.target.value);
-  coneSource.setResolution(resolution);
-  renderWindow.render();
-});
+
+global.reader = reader;
+global.renderer = renderer;
+global.polyMapper = polyMapper;
+global.renderWindow = renderWindow;
