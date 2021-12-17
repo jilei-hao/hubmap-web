8bdabaf1f43342cac3c9de5ce38cb895452e0c85
jilei-hao <jilei.cs@gmail.com>
2021-12-17 01:36:51 -0500

    hubmap-1: added control panel logic

--- src/index.js
+++ src/index.js
@@ -32,52 +32,19 @@ const polyActor = vtkActor.newInstance();
 const reader = vtkPolyDataReader.newInstance();
 
 // url of the file server, should be in a configuration file
-const server_url = 'http://192.168.4.23:3000';
-const file_name = 'Ovary.vtk';
+const server_url = 'http://192.168.4.23:3000/index.js?ns=2&nr=4&d=20&h=30&w=40';
+//const file_name = 'Ovary.vtk';
 
 // connect the rendering pipeline
 polyMapper.setInputConnection(reader.getOutputPort());
 polyActor.setMapper(polyMapper);
 renderer.addActor(polyActor);
-
-reader.setUrl(`${server_url}/${file_name}`).then(() => {
-  
-  reader.loadData().then(() => {
-    let polyData = reader.getOutputData();
-    let pointData = polyData.getPointData();
-
-    // Copy scalar data into color array
-    // - vtk.js color rendering by scalar only works for Int32Array
-    let pts = polyData.getPoints();
-    const clr = new Int32Array(pts.getNumberOfPoints());
-    const orig = pointData.getArrayByName('Label').getData();
-    for (let i = 0; i < clr.length; ++i) {
-      clr[i] = orig[i];
-    }
-
-    polyMapper.setScalarRange(0, 16);
-    
-    pointData.setScalars(
-      vtkDataArray.newInstance({
-        name: 'color',
-        numberOfComponents: 1,
-        values: clr,
-      })
-    )
-
-    console.log('color scalars created');
-
-    // --Debug: export local variable to global scope
-    global.polyData = polyData;
-    global.pointData = pointData;
-
-    renderer.resetCamera();
-    renderWindow.render();
-  });
-});
-
 renderer.resetCamera();
-renderWindow.render();
+
+// render default mesh when opening page
+window.addEventListener('load', () => {
+  document.getElementById('Create').click();
+});
 
 // -----------------------------------------------------------
 // UI control handling
@@ -87,6 +54,9 @@ const controlPanel = `
 <table>
   <tr>
     <td>
+      <label>Representation</label>
+    </td>
+    <td>
       <select class="representations" style="width: 100%">
         <option value="0">Points</option>
         <option value="1">Wireframe</option>
@@ -94,6 +64,63 @@ const controlPanel = `
       </select>
     </td>
   </tr>
+  <tr>
+    <td>
+      <label> Number of Sagittal Slices
+      </label>
+    </td>
+    <td>
+      <input type="number" id="ns" value=2>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <label> Number of Circumferential Slices
+      </label>
+    </td>
+    <td>
+      <input type="number" id="nr" value=4>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <label> Anterior-Posterior Distance (mm)
+        
+      </label>
+    </td>
+    <td>
+      <input type="text" id="d" value=20>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <label> Superior-Inferior Distance (mm)
+        
+      </label>
+    </td>
+    <td>
+      <input type="text" id="h" value=30>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <label> Medial-Lateral Distance (mm)
+        
+      </label>
+    </td>
+    <td>
+      <input type="text" id="w" value=40>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <button id="Create">Create</button>
+      <button id="Download">Download STL File</button>
+    </td>
+    <td>
+    </td>
+  </tr>
+
 </table>
 `;
 
@@ -104,6 +131,56 @@ representationSelector.addEventListener('change', (e) => {
   const newRepValue = Number(e.target.value);
   polyActor.getProperty().setRepresentation(newRepValue);
   renderWindow.render();
+});
+
+const btnCreate = document.getElementById('Create');
+
+btnCreate.addEventListener('click', () => {
+  const ns = document.getElementById('ns').value;
+  const nr = document.getElementById('nr').value;
+  const d = document.getElementById('d').value;
+  const h = document.getElementById('h').value;
+  const w = document.getElementById('w').value;
+
+  const server_url = `http://192.168.4.23:3000/index.js?ns=${ns}&nr=${nr}&d=${d}&h=${h}&w=${w}`;
+
+  console.log("server_url: ", server_url);
+
+  reader.setUrl(server_url).then(() => {
+    reader.loadData().then(() => {
+      let polyData = reader.getOutputData();
+      let pointData = polyData.getPointData();
+  
+      // Copy scalar data into color array
+      // - vtk.js color rendering by scalar only works for Int32Array
+      let pts = polyData.getPoints();
+      const clr = new Int32Array(pts.getNumberOfPoints());
+      const orig = pointData.getArrayByName('Label').getData();
+      for (let i = 0; i < clr.length; ++i) {
+        clr[i] = orig[i];
+      }
+  
+      polyMapper.setScalarRange(0, 16);
+      
+      pointData.setScalars(
+        vtkDataArray.newInstance({
+          name: 'color',
+          numberOfComponents: 1,
+          values: clr,
+        })
+      )
+  
+      console.log('color scalars created');
+  
+      // --Debug: export local variable to global scope
+      global.polyData = polyData;
+      global.pointData = pointData;
+  
+      renderWindow.addRenderer(renderer);
+      renderer.resetCamera();
+      renderWindow.render();
+    });
+  });
 });
 
 
